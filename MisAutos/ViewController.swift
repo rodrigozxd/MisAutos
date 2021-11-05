48847205c96cd117270dd178bf7835ad932f6b4c
rodrigozxd <rodrigo.paredes@tecsup.edu.pe>
2021-11-05 16:23:37 -0700

    Initial Commit

---
+++ MisAutos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MisAutos
+//
+//  Created by RodrigoParedes on 5/11/21.
+//  Copyright © 2021 RodrigoParedes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
